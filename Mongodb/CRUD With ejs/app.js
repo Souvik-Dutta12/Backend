34bbddb810d3c76ead319979b64624466e30293c
Souvik Dutta <souvikdutta5122005@gmail.com>
2025-01-26 12:43:55 +0530

    crud with ejs

---
+++ Mongodb/CRUD With ejs/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const userModel = require("./models/user");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+//for set up the ejs
+app.set('view engine', 'ejs');
+
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.get("/read", async (req,res)=>{
+    let users = await userModel.find();
+    res.render("read",{users});
+})
+
+app.post("/create", async (req,res)=>{
+    let {name,email,image} = req.body;
+
+    let createdUser = await userModel.create({
+        name,
+        email,
+        image
+    });
+    res.send(createdUser);
+})
+
+app.listen(3000);
