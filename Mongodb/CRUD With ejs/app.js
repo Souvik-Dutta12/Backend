2c8bd79e2da62657e4ddd23a51ecf20c5640136a
Souvik Dutta <souvikdutta5122005@gmail.com>
2025-01-26 19:12:32 +0530

    Add a edit feature

--- Mongodb/CRUD With ejs/app.js
+++ Mongodb/CRUD With ejs/app.js
@@ -18,6 +18,15 @@ app.get("/read", async (req,res)=>{
     let users = await userModel.find();
     res.render("read",{users});
 })
+app.get("/edit/:userid", async (req,res)=>{
+    let user = await userModel.findOne({_id:req.params.userid});
+    res.render("edit",{user});
+})
+app.post("/update/:userid", async (req,res)=>{
+    let {name,email,image} = req.body;
+    let user = await userModel.findOneAndUpdate({_id:req.params.userid}, {name,email,image},{new:true});
+    res.redirect("/read",{user});
+})
 
 app.post("/create", async (req,res)=>{
     let {name,email,image} = req.body;
